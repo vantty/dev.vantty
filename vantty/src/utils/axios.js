7b19e14f7e16feeffb67ad734ac9d3ea3a57ef33
Julian Urrego <julian.urrego.ar@gmail.com>
2019-10-02 13:48:44 -0400

    Deploy vantty.ca

--- vantty/src/utils/axios.js
+++ vantty/src/utils/axios.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-// export const server = axios.create({
-//   baseURL: "https://apivantty.now.sh/api/"
-// });
+export const server = axios.create({
+  baseURL: "https://apivantty.now.sh/api/"
+});
 
-// export const API_URL = "https://apivantty.now.sh/api";
+export const API_URL = "https://apivantty.now.sh/api";
 
-//Local
+//for all
 export const elastic = axios.create({
   baseURL: "https://scalr.api.appbase.io/vantty-database/_doc/"
 });
 
-export const server = axios.create({
-  baseURL: "http://localhost:5000/api/"
-});
+//Local
+// export const server = axios.create({
+//   baseURL: "http://localhost:5000/api/"
+// });
 
-export const API_URL = "http://localhost:5000/api";
+// export const API_URL = "http://localhost:5000/api";
