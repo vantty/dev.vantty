0f85ffe8c9fb6cd3877abe3fc378e929d1e43318
Julian Urrego <julian.urrego.ar@gmail.com>
2019-10-12 18:44:58 -0400

    Add: good way axios

--- vantty/src/utils/axios.js
+++ vantty/src/utils/axios.js
@@ -1,39 +1,12 @@
-// import axios from "axios";
-
-// // For all
-// export const elastic = axios.create({
-//   baseURL: process.env.REACT_APP_ELASTIC_BASE_URL
-// });
-
-// // // Now
-// export const server = axios.create({
-//   baseURL: process.env.REACT_APP_NOW_BASE_URL
-// });
-// export const API_URL = process.env.REACT_APP_NOW_API_URL;
-
-// Local;
-// export const server = axios.create({
-//   baseURL: process.env.REACT_APP_LOCAL_BASE_URL
-// });
-
-// export const API_URL = process.env.REACT_APP_LOCAL_API_URL;
-
 import axios from "axios";
 
 // For all
 export const elastic = axios.create({
-  baseURL: "https://scalr.api.appbase.io/vantty-database/_doc/"
+  baseURL: process.env.REACT_APP_ELASTIC_BASE_URL
 });
 
 // // Now
 export const server = axios.create({
-  baseURL: "https://apivantty.now.sh/api/"
+  baseURL: process.env.REACT_APP_NOW_BASE_URL
 });
-export const API_URL = "https://apivantty.now.sh/api";
-
-// Local;
-// export const server = axios.create({
-//   baseURL: "http://localhost:5000/api/"
-// });
-
-// export const API_URL = "http://localhost:5000/api";
+export const API_URL = process.env.REACT_APP_NOW_API_URL;
