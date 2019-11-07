c07bbe525bae8df14185957138fac75d8b63562a
Julian Urrego <julian.urrego.ar@gmail.com>
2019-11-06 22:33:50 -0500

    Add: stripe

--- vantty/src/views/Book/index.jsx
+++ vantty/src/views/Book/index.jsx
@@ -1,15 +1,86 @@
-import React from "react";
-import { StripeProvider, Elements } from "react-stripe-elements";
+import React, { Component } from "react";
+import {
+  CardElement,
+  injectStripe,
+  StripeProvider,
+  Elements
+} from "react-stripe-elements";
 
-// Components
-import StripeForm from "./components/StripeForm";
+const log = console.log;
+// You can customize your Elements to give it the look and feel of your site.
+const createOptions = () => {
+  return {
+    style: {
+      base: {
+        fontSize: "16px",
+        color: "#424770",
+        fontFamily: "Open Sans, sans-serif",
+        letterSpacing: "0.025em",
+        "::placeholder": {
+          color: "#aab7c4"
+        }
+      },
+      invalid: {
+        color: "#c23d4b"
+      }
+    }
+  };
+};
 
-export default function index() {
-  return (
-    <StripeProvider apiKey="pk_test_HHkuqizzDlMmZMauV39jofnJ000LOHKMAN">
-      <Elements>
-        <StripeForm />
-      </Elements>
-    </StripeProvider>
-  );
+class _CardForm extends Component {
+  state = {
+    errorMessage: ""
+  };
+
+  handleChange = ({ error }) => {
+    if (error) {
+      this.setState({ errorMessage: error.message });
+    }
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    if (this.props.stripe) {
+      this.props.stripe
+        .createToken()
+        .then(this.props.handleResult)
+        .then(token => {
+          log(token.token.id);
+        });
+    } else {
+      console.log("Stripe.js hasn't loaded yet.");
+    }
+  };
+
+  render() {
+    return (
+      <div className='CardDemo'>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>
+            Card details
+            <CardElement onChange={this.handleChange} {...createOptions()} />
+          </label>
+          <div className='error' role='alert'>
+            {this.state.errorMessage}
+          </div>
+          <button>Pay</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+const CardForm = injectStripe(_CardForm);
+
+export class CardDemo extends Component {
+  render() {
+    return (
+      <StripeProvider apiKey={"pk_test_HHkuqizzDlMmZMauV39jofnJ000LOHKMAN"}>
+        <Elements>
+          <CardForm handleResult={this.props.handleResult} />
+        </Elements>
+      </StripeProvider>
+    );
+  }
+}
+export default CardDemo;
