92481f2bc58663c5e75019aa0fcf1a14d662930f
Sebastian Hernandez <sebhernandezram@gmail.com>
2019-12-19 22:35:49 -0500

    Templates works fine in local. Deploy to Now does not work

--- api.vantty/helpers/index.js
+++ api.vantty/helpers/index.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const log = console.log;
 
 exports.validator = (req, res, next) => {
   req.check("firstName", "Please enter your first name").notEmpty();
@@ -59,8 +61,42 @@ exports.profileValidatorPortfolio = (req, res, next) => {
 };
 
 // Compose and send email
+// exports.composeEmail = (email, subject, html) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.mailgun.org",
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         user: "postmaster@mg.vantty.ca",
+//         pass: "a0786ff2f0af6c7bc33de732df6b9202-2dfb0afe-a4ab1b23"
+//       }
+//     });
+
+//     let message = {
+//       from: "admin@vantty.ca",
+//       to: `${email}`,
+//       subject: subject,
+//       html: html
+//     };
+
+//     transporter.sendMail(message, (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("Email sent");
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send("Server error");
+//   }
+// };
+
+// Email Template
 exports.composeEmail = (email, subject, html) => {
   try {
+    // Step 1
     const transporter = nodemailer.createTransport({
       host: "smtp.mailgun.org",
       port: 465,
@@ -71,90 +107,44 @@ exports.composeEmail = (email, subject, html) => {
       }
     });
 
-    let message = {
-      from: "admin@vantty.ca",
-      to: `${email}`,
-      subject: subject,
-      html: html
+    // Step 2
+    transporter.use(
+      "compile",
+      hbs({
+        viewEngine: {
+          extName: ".hbs",
+          partialsDir: "./views",
+          layoutsDir: "./views",
+          defaultLayout: "index.hbs"
+        },
+        viewPath: "./views",
+        extName: ".hbs"
+      })
+    );
+
+    // Step 3
+    let mailOptions = {
+      from: "admin@vantty.ca", // TODO: email sender
+      to: `${email}`, // TODO: email receiver
+      subject: "Nodemailer - Test",
+      text: "Wooohooo it works!!",
+      template: "index",
+      context: {
+        subject: subject,
+        html: html
+      }
     };
 
-    transporter.sendMail(message, (err, data) => {
+    // Step 4
+    transporter.sendMail(mailOptions, (err, data) => {
       if (err) {
-        console.log(err);
-      } else {
-        console.log("Email sent");
+        log("EMAIL ERROR", err);
+        return log("Error occurs");
       }
+      return log("Email sent!!!");
     });
   } catch (error) {
     console.log(error);
     res.status(500).send("Server error");
   }
 };
-
-// Compose Email
-// exports.composeEmail = (email, subject, html) => {
-//   let users = [
-//     {
-//       name: "Jack",
-//       email: "sebhernandezram@gmail.com"
-//     }
-//   ];
-
-//   function sendEmail(obj) {
-//     return transporter.sendMail(obj);
-//   }
-
-//   function loadTemplate(templateName, contexts) {
-//     let template = new EmailTemplate(
-//       path.join(__dirname, "templates", templateName)
-//     );
-//     return Promise.all(
-//       contexts.map(context => {
-//         return new Promise((resolve, reject) => {
-//           template.render(context, (err, result) => {
-//             if (err) reject(err);
-//             else
-//               resolve({
-//                 email: result,
-//                 context
-//               });
-//           });
-//         });
-//       })
-//     );
-//   }
-
-//   loadTemplate("updates-april-2017", users)
-//     .then(results => {
-//       return Promise.all(
-//         results.map(result => {
-//           sendEmail({
-//             to: result.context.email,
-//             from: "Me :)",
-//             subject: result.email.subject,
-//             html: result.email.html,
-//             text: result.email.text
-//           });
-//         })
-//       );
-//     })
-//     .then(() => {
-//       console.log("Yay!");
-//     });
-// };
-
-// exports.createReviewValidator = (req, res, next) => {
-//   req.check("author", "Please enter your name").notEmpty();
-//   req
-//     .check("body", "Please enter a review")
-//     .notEmpty()
-//     .isLength({ min: 5, max: 300 })
-//     .withMessage("The review must be between 5 and 300 characters");
-
-//   const errors = req.validationErrors();
-//   if (errors) {
-//     return res.status(400).json({ errors });
-//   }
-
-//   next();
-// };
