b42acbabc59dc04d00069f2500ad9d451d9dc6cb
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-02-07 10:17:22 -0500

    Fix newCardObj function + HomeDescription IN MASTER BRANCH

--- vantty/src/views/Home/components/index.js
+++ vantty/src/views/Home/components/index.js
@@ -2,3 +2,4 @@ export { default as HomeBanner } from "./HomeBanner";
 export { default as HomeCarousel } from "./HomeCarousel";
 export { default as HomeGrid } from "./HomeGrid";
 export { default as HomeJumbotron } from "./HomeJumbotron";
+export { default as HomeDescription } from "./HomeDescription";
