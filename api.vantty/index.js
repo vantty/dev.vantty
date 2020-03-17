123ff679562faa70e64cb174cd434ccf698a8b8f
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-03-17 15:52:40 -0500

    cors options in back

--- api.vantty/index.js
+++ api.vantty/index.js
@@ -51,7 +51,7 @@ var corsOptions = {
 app.use(morgan("dev"));
 app.use(expressValidator());
 app.use(express.json({ extended: false }));
-app.use(cors("*"));
+app.use(cors(corsOptions));
 app.use(formData.parse());
 
 // Routes
@@ -82,16 +82,3 @@ app.use((error, req, res, next) => {
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
-
-// Felipe Comments
-// getAll should be using pagination. Use in profile, book, user, .... upset (in mongo:skip), limit 10, sort = data organizada. result = object: items \\\\\ Example:    const review = await Review.find().sort({ date: -1 });
-// sort children in mongo - find
-// remove unnecesary comment Exm: connect server NOO
-// create CONSTANTS. Helpers has an example. Move emailTypes to this file.
-// Email service should be management by other; mailChip, ....
-
-//FRONT
-// Lista de formularios
-// Implement formik: why? 1. it manages states | 2.
-// Loading... => probably template
-// Code split => if I enter a profile, the charge should be once.
