16aef0c5a29ee67f487a8c3cc126fd5a31b03ea0
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-03-27 12:17:33 -0500

    Modify FAQ text

--- vantty/src/views/LandingPage/FAQList.js
+++ vantty/src/views/LandingPage/FAQList.js
@@ -1,84 +1,83 @@
 export const faq = [
   {
-    question: "Is it available in my city?",
-    answer: "Vantty is now currently available in Toronto-ON"
+    question: 'Is it available in my city?',
+    answer: 'Vantty is now currently available in Toronto-ON'
   },
   {
-    question: "Who can apply to become an partnered artist on Vantty?",
+    question: 'Who can apply to become an partnered artist on Vantty?',
     answer:
-      "Anyone who is a certified makeup artist or hairstylist, or has enough work experience, is welcome to become a partnered artist with Vantty."
+      'Anyone who is a certified makeup artist or hairstylist, or has enough work experience, is welcome to become a partnered artist with Vantty.'
   },
   {
     question:
-      "What are some of the reasons  my profile  might not be approved?",
+      'What are some of the reasons  my profile  might not be approved?',
     answer:
-      "Your profile might to be approved for reasons including the following, but not limited to: Inappropriate pictures/content or False information on the profile"
+      'Your profile might to be approved for reasons including the following, but not limited to: Inappropriate pictures/content or False information on the profile'
   },
   {
     question:
-      "What are the different types of  services that I can offer through Vantty beauty?",
+      'What are the different types of  services that I can offer through Vantty beauty?',
     answer:
-      "With Vantty  you can offer all  types of professional beauty services, but  mainly makeup and hairstyling. We are focused on providing professional beauty services such as Makeup: Social Bridal, Glam, and Hair style services like: Haircuts, All-Over color, Formal styling/Special Occasion hair, Blowouts, Deep conditioning treatments"
+      'With Vantty  you can offer all  types of professional beauty services, but  mainly makeup and hairstyling. We are focused on providing professional beauty services such as Makeup: Social Bridal, Glam, and Hair style services like: Haircuts, All-Over color, Formal styling/Special Occasion hair, Blowouts, Deep conditioning treatments'
   },
   {
-    question: "What happened if my client cancels?",
+    question: 'What happened if my client cancels?',
     answer:
-      "If your client cancels their appointment, we will notify you and automatically unblock the dates on your calendar, so that you can provide your services to  other users."
+      'If your client cancels their appointment, we will notify you and automatically unblock the dates on your calendar, so that you can provide your services to  other users.'
   },
   {
-    question: "How we manage cancellation policies?",
+    question: 'How we manage cancellation policies?',
     answer:
-      "Any customer´s cancellations done 4 hours before the appointment, will result  in  you receiving the 80% of the full price of the service."
+      'Any customer´s cancellations done 4 hours before the appointment, will result  in  you receiving the 80% of the full price of the service.'
   },
   {
-    question: "How do I cancel a booking as a partnered artist?",
+    question: 'How do I cancel a booking as a partnered artist?',
     answer:
-      "We strongly recommend and encourage you to only take requests you can accommodate to avoid cancellations and keep your business growing."
+      'We strongly recommend and encourage you to only take requests you can accommodate to avoid cancellations and keep your business growing.'
   },
   {
-    question: "What should I do if I need to cancel my service?.",
+    question: 'What should I do if I need to cancel my service?.',
     answer:
-      "In case you do need to cancel, please contact your client first to let them know. Then, cancel the service in your Vantty account. "
+      'In case you do need to cancel, please contact your client first to let them know. Then, cancel the service in your Vantty account. '
   },
   {
-    question: "What happen If I cancel too many services?",
+    question: 'What happen If I cancel too many services?',
     answer:
       "We encourage our partnered artist  to be aware of costumers' appointments, for some of them the provision of the service may determine important events and meetings."
   },
   {
     question:
-      "What if I need to cancel because of an emergency or unavoidable circumstance?",
+      'What if I need to cancel because of an emergency or unavoidable circumstance?',
     answer:
-      "If there is an unexpected circumstance that is out of your control, please contact our team for more information."
+      'If there is an unexpected circumstance that is out of your control, please contact our team for more information.'
   },
   {
-    question: "What if I am late?",
+    question: 'What if I am late?',
     answer:
-      "If you are late, contact your client as soon as possible  and let them know about the situation. Remember, clients have scheduled their appointments around their personal agendas and they can be affected due to unpunctuality, resulting in poorer reviews for your profile."
+      'If you are late, contact your client as soon as possible  and let them know about the situation. Remember, clients have scheduled their appointments around their personal agendas and they can be affected due to unpunctuality, resulting in poorer reviews for your profile.'
   },
   {
-    question: "How reviews work in Vantty?",
+    question: 'How reviews work in Vantty?',
     answer:
-      "Once the service is finished, your client will received an email asking for a review. We strongly encourage our partnered artist to provide excellent experiences in all beauty services areas on their profiles."
+      'Once the service is finished, your client will received an email asking for a review. We strongly encourage our partnered artist to provide excellent experiences in all beauty services areas on their profiles.'
   },
   {
-    question: "How do I edit my profile?",
+    question: 'How do I edit my profile?',
     answer:
-      "Log in your account, Go to your profile, Click on settings and you will be able to change and edit your profile."
+      'Log in your account, Go to your profile, Click on settings and you will be able to change and edit your profile.'
   },
   {
-    question: "How do I get paid for appointments ?",
+    question: 'How do I get paid for appointments ?',
     answer:
-      "The payment you will receive is your service fee, minus the Vantty service fee and  the cost of the banking transaction from stripe. To view information about any of your payments, click on the “banking info” button in your Vantty profile. All payments are done through Stripe’s payment processing platform and will be sent out weekly."
+      'The payment you will receive is your service fee, minus the Vantty service fee and  the cost of the banking transaction from stripe. To view information about any of your payments, click on the “banking info” button in your Vantty profile. All payments are done through Stripe’s payment processing platform and will be sent out weekly.'
   },
   {
-    question: "Where can I see all services I have done?",
+    question: 'Where can I see all services I have done?',
     answer:
-      "A record with all your previous services, can be found  in your “Bookings” menu."
+      'A record with all your previous services, can be found  in your “Bookings” menu.'
   },
   {
-    question: "What is the Vantty service fee?",
-    answer:
-      "The Vantty service fee is a standard of 20%, that is charged on all services."
+    question: 'What is the Vantty service comission?',
+    answer: `The Vantty service comission is a standard of 20%, that is charged on all services. If you're one of the first 30 partnerd artist signed into our page, you will have to pay NO COMISSION for 3 months; but don't worry, this will apply only after COVID-19 quarantine ends!`
   }
 ];
--- vantty/src/views/LandingPage/Sections/ProductSection.js
+++ vantty/src/views/LandingPage/Sections/ProductSection.js
@@ -1,56 +1,55 @@
-import React from "react";
+import React from 'react';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 // @material-ui/icons
 // import Chat from "@material-ui/icons/Chat";
-import Today from "@material-ui/icons/Today";
-import Review from "@material-ui/icons/RateReview";
-import Money from "@material-ui/icons/MonetizationOn";
-import Payment from "@material-ui/icons/Payment";
+import Today from '@material-ui/icons/Today';
+import Review from '@material-ui/icons/RateReview';
+import Money from '@material-ui/icons/MonetizationOn';
+import Payment from '@material-ui/icons/Payment';
 
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
+import VerifiedUser from '@material-ui/icons/VerifiedUser';
+import Fingerprint from '@material-ui/icons/Fingerprint';
 // core components
-import GridContainer from "../components/GridContainer";
-import GridItem from "../components/GridItem";
-import InfoArea from "../components/InfoArea";
-import { Typography } from "@material-ui/core";
+import GridContainer from '../components/GridContainer';
+import GridItem from '../components/GridItem';
+import InfoArea from '../components/InfoArea';
+import { Typography } from '@material-ui/core';
 
 export default function ProductSection() {
- 
   const useStyles = makeStyles(theme => ({
     section: {
-      padding: "70px 0",
-      textAlign: "center"
+      padding: '70px 0',
+      textAlign: 'center'
     },
     title: {
-      color: "#3C4858",
-      margin: "1.75rem 0 0.875rem",
-      marginBottom: "1rem",
-      marginTop: "30px",
-      minHeight: "32px",
-      textDecoration: "none"
+      color: '#3C4858',
+      margin: '1.75rem 0 0.875rem',
+      marginBottom: '1rem',
+      marginTop: '30px',
+      minHeight: '32px',
+      textDecoration: 'none'
     },
     description: {
-      color: "#999",
+      color: '#999',
       marginBottom: theme.spacing(2)
     }
   }));
   const classes = useStyles();
   return (
     <div className={classes.section}>
-      <GridContainer justify='center'>
+      <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
-          <Typography variant='h2' className={classes.title}>
+          <Typography variant="h2" className={classes.title}>
             What is Vantty?
           </Typography>
           {/* <h5 className={classes.description}> */}
-          <Typography variant='h4' className={classes.description}>
+          <Typography variant="h4" className={classes.description}>
             Vantty is a beauty platform which uses an advanced search engine to
-            connect beauty artist with clients. Anywhere, Anytime.{" "}
+            connect beauty artist with clients. Anywhere, Anytime.{' '}
             <strong>
-              Our goal its to grow your personal brand and stand you out.
+              Our goal it's to grow your personal brand and stand you out.
             </strong>
           </Typography>
           {/* </h5> */}
@@ -60,55 +59,55 @@ export default function ProductSection() {
         <GridContainer>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Online Booking'
-              description='Your clients can book your services in 3 easy steps. You will be notified via email to accept the service.'
+              title="Online Booking"
+              description="Your clients can book your services in 3 easy steps. You will be notified via email to accept the service."
               icon={Today}
-              iconColor='pink'
+              iconColor="pink"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Get Reviews'
-              description='Once you have completed the service, your client will leave a review about her experience.'
+              title="Get Reviews"
+              description="Once you have completed the service, your client will leave a review about her experience."
               icon={Review}
-              iconColor='purple'
+              iconColor="purple"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Advertising'
-              description='We create exclusive campaigns in Facebook, Instagram and Google for you to increase your clients.'
+              title="Advertising"
+              description="We create exclusive campaigns in Facebook, Instagram and Google for you to increase your clients."
               icon={Money}
-              iconColor='green'
+              iconColor="green"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Secure and Fast payments'
-              description='Your clients will pay using their credit card, and in 7 days you will receive your money in your banck account.'
+              title="Secure and Fast payments"
+              description="Your clients will pay using their credit card, and in 7 days you will receive your money in your banck account."
               icon={Payment}
-              iconColor='purple'
+              iconColor="purple"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Professional Profile'
-              description='You will have a dedicated profile where you can show your experience, upload your portfolio and save client reviews.'
+              title="Professional Profile"
+              description="You will have a dedicated profile where you can show your experience, upload your portfolio and save client reviews."
               icon={VerifiedUser}
-              iconColor='green'
+              iconColor="green"
               vertical
             />
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title='Direct Chat'
-              description='After accepting the service, you can chat wuth your client via Whatsapp or SMS.'
+              title="Direct Chat"
+              description="After accepting the service, you can chat wuth your client via Whatsapp or SMS."
               icon={Fingerprint}
-              iconColor='pink'
+              iconColor="pink"
               vertical
             />
           </GridItem>
