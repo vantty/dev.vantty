cd54711c4495ba67e6b6888117249a2b3938e873
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-04-20 11:42:57 -0500

    Change cors options. Add NODE_ENV dev

--- api.vantty/config/cors.js
+++ api.vantty/config/cors.js
@@ -1,11 +1,7 @@
 const cors = require('cors');
 
 const corsConfig = function () {
-  const whitelist = [
-    'http://localhost:3000',
-    'https://vantty.ca',
-    'https://www.vantty.ca',
-  ];
+  const whitelist = ['https://vantty.ca', 'https://www.vantty.ca'];
 
   const corsOptions = {
     origin: function (origin, callback) {
